1cfb5f7ff13b720aaa35551c5576185cc249f173
no-Seaweed <40819442+no-Seaweed@users.noreply.github.com>
2019-04-27 15:17:32 -0400

    Add files via upload

--- CSE389_Final/js/map.js
+++ CSE389_Final/js/map.js
@@ -1,9 +1,12 @@
-    function initMap() {
+function initMap() {
+    //initiate map
+
         var map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 43.0399089, lng: -76.1324457},
           zoom: 16,
           zoomControl: true,
           disableDefaultUI: true,
+          zoomControl: false,
           styles: [
             {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
             {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
@@ -96,31 +99,94 @@
             scaledSize: new google.maps.Size(60,60),
             origin: new google.maps.Point(0,0),
             anchor: new google.maps.Point(0,0)
+            },
+            {
+            url: '../pics/Ellipse_31.png',
+            scaledSize: new google.maps.Size(40,40),
+            origin: new google.maps.Point(0,0),
+            anchor: new google.maps.Point(0,0)
             }
         ]
-        var markers = [
+    //series of markers
+        var SaleMarkers = [
             {
             coords:{lat: 43.0399089, lng: -76.1324457},
             image:icon[0],
-            content:'<h1>description goes here</h1>'
+            content:'<h1>some notation goes here</h1>'
             },
             {
             coords:{lat: 43.0366496, lng: -76.1341085},
             image:icon[0],
-            content:'<h1>description goes here</h1>'
-           },
+            content:'<h1>some notation goes here</h1>'
+           }
+        ]
+        
+        var SpinMarkers = [
             {
             coords:{lat: 43.037659, lng: -76.134907},
             image:icon[1],
-            content:'<h1>description goes here</h1>'
+            content:'<h1>some notation goes here</h1>'
             }
         ]
-       
-        for(var i = 0; i < markers.length; i++){
-            addMarker(markers[i]);
+        
+        var EventMarkers = [
+            {
+            coords:{lat: 43.0354257, lng: -76.140964},
+            image:icon[1],
+            content:'<h1>some notation goes here</h1>'
+            }
+        ]
+        
+        var generalMarkers = [
+            {
+                coords:{lat: 43.0380994, lng: -76.1327453},
+                image:icon[2], 
+                content:'<h1>some notation goes here</h1>'
+            },
+            {
+                coords:{lat: 43.0400993, lng: -76.1372352},
+                image:icon[2], 
+                content:'<h1>some notation goes here</h1>'
+            }
+        ]
+        for(var i = 0; i < generalMarkers.length; i++){
+                addMarker(generalMarkers[i]);
+        }
+    //functions to print markers        
+        function filterSale(){
+            for(var i = 0; i < SaleMarkers.length; i++){
+                addMarker(SaleMarkers[i]);
+            }
         }
         
-
+        function filterSpin(){
+            for(var i = 0; i < SpinMarkers.length; i++){
+                addMarker(SpinMarkers[i]);
+            }
+        }
+    
+        function filterEvent(){
+            for(var i = 0; i < EventMarkers.length; i++){
+                addMarker(EventMarkers[i]);
+            }
+        }
+        this.activateFilter = function(cls){
+            if(document.getElementsByClassName(cls)[0].innerText=="Spin"){
+                window.sale = null;
+                window.event = null;
+                filterSpin();
+            }
+            else if(document.getElementsByClassName(cls)[0].innerText=="Party")
+                filterSale();
+            else if(document.getElementsByClassName(cls)[0].innerText=="Event"){
+                filterEvent();
+            }else{
+                filterSpin();
+                filterSale();
+                filterEvent();
+            }
+        } 
+        
         function addMarker(props){
             var marker = new google.maps.Marker({
                 position:props.coords,
@@ -144,7 +210,8 @@
                 }); 
             }
         }
-        
+    
+
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
             var pos = {
@@ -161,6 +228,8 @@
               handleLocationError(false, map.getCenter());
             }
         }
+
+        
         function handleLocationError(browserHasGeolocation, pos) {
             console.log('Error: The Geolocation service failed.');
             }
